527f0bf8c120709e561675dd9621cf6b0c695cad
2405-011-pw <walypa56@gmail.com>
2024-06-27 13:48:00 +0100

    made 1 qr code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
+import QRCode from "qrcode";
 
 function App() {
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [myDataURL, setMyDataURL] = React.useState("");
+
+  function handleChange(event) {
+    setSearchTerm(event.target.value);
+  }
+  function generateQRCode() {
+    QRCode.toDataURL(searchTerm, function (err, searchTerm) {
+      console.log(myDataURL);
+      setMyDataURL(searchTerm);
+    })
+  }
 
   return (
     <>
+      <h1>QR Code Generator</h1>
+      <input type="text" placeholder="Type something..." onChange={handleChange} />
+      <br />
+      <button onClick={generateQRCode}>Generate</button>
+      <div className="qr-code">
+        <img src={myDataURL}></img>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
